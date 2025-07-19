1273ab44a4f60fed0a9a7e2150e8903f61de0b3f
thanu <thanu@Venkat_TRV.lan>
2025-07-18 19:15:07 -0500

    new class

---
+++ testartId/src/main/java/testartId/TestClass.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package testartId;
+
+/**
+ * 
+ */
+public class TestClass {
+
+	/**
+	 * 
+	 */
+	public TestClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

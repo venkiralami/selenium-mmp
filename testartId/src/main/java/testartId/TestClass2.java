ca5bba847356b3933d31136602cc31848f7b5a26
venkiralami <venki.ralami@gmail.com>
2025-07-25 15:12:23 -0500

    added change for release v1.1

---
+++ testartId/src/main/java/testartId/TestClass2.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package testartId;
+
+/**
+ * 
+ */
+public class TestClass2 {
+
+	/**
+	 * 
+	 */
+	public TestClass2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Test Java Program 2 executed Successfully");
+		System.out.println("This is the second test class in the project");
+		System.out.println("Adding some more lines to this class for testing");
+	}
+
+}
